f94edd3c3b9ab9a8e120ac8645c1ab5cae4c000d
John Judeh <john.judeh@originmarkets.com>
2021-06-17 18:27:06 +0100

    feat: Add docstrings to each function in the create_termsheet.py file

--- create_termsheet.py
+++ create_termsheet.py
@@ -16,23 +16,42 @@ import json
 from docxtpl import DocxTemplate
 
 BASE_DIR = Path(__file__).parent
-TERMSHEET_TEMPLATE_PATH = BASE_DIR / "termsheet_template.docx"
+TERMSHEET_TEMPLATE_PATH = BASE_DIR / "Machine-Readable Termsheet.docx"
 
 ARGNAME_INPUT_JSON_PATH = "json_input_file"
 ARGNAME_OUTPUT_DOCX_PATH = "docx_output_file"
 
 
-def validate_file_type(parser, filepath, ext, must_exist=True):
-    path = Path(filepath)
+def validate_file_type(parser, file_path, ext, must_exist=True):
+    """
+    Validates file path string passed to an argparse parser. Checks that string has a valid
+    extension and is a file that exists if must_exist = True. Raises validation errors
+    if any of the conditions fail.
+
+    Args:
+        parser: An argparse.ArgumentParser instance used to raise validation errors
+        file_path: The file path string to be validated
+        ext: The file extenion to validate the file path against. In the format '.*'
+        must_exist: Boolean for whether to check existence of file in validation. Defaults to True
+
+    Returns:
+        str | None: The string passed to it if its valid, otherwise None
+    """
+    path = Path(file_path)
     if must_exist and not (path.exists() and path.is_file):
-        parser.error(f"The file {filepath} does not exist")
+        parser.error(f"The file {file_path} does not exist")
     elif not path.suffix == ext:
-        parser.error(f"The file {filepath} has the wrong extension. Expected {ext}")
-    else:
-        return filepath
+        parser.error(f"The file {file_path} has the wrong extension. Expected {ext}")
+    return file_path
 
 
 def create_arg_parser():
+    """
+    Creates an argument parser to be used when the file is run directly as a command line tool.
+
+    Returns:
+        argparse.ArgumentParser: A parser to be used to parse arguments from sys.argv
+    """
     DESCRIPTION = (
         "A command line tool to create a termsheet using "
         "the Origin termsheet template with an Airbrush-compliant "
@@ -53,6 +72,13 @@ def create_arg_parser():
 
 
 def create_termsheet(input_file_path, output_file_path):
+    """
+    Create a termsheet in docx format using the Origin template and a json input file.
+
+    Args:
+        input_file_path: A json input file path to render the termsheet with
+        output_file_path: A docx output file path to write to on success
+    """
     with open(input_file_path, "r") as f:
         termsheet_fields = json.load(f)
 
